3b26ac4ac9c9930f04c00b14fa2bc44aff0f2d96
Purp1e <438518244@qq.com>
2023-08-16 16:26:58 +0800

    fix ref problem

--- src/components/TodoAdd.vue
+++ src/components/TodoAdd.vue
@@ -1,37 +1,39 @@
 <template>
-      <div class = "input-add">
-        <input type = "text" name = "todo" v-model="todoContent" />
-        <button>
-          <i class = "plus"></i>
-        </button>
-      </div>
+  <div class="input-add">
+    <input type="text" name="todo" v-model="todoContent" />
+    <button>
+      <i class="plus"></i>
+    </button>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "TodoAdd",
-        setup(props, context) {
-          const todoContent = ref("");
+import { ref } from "vue";
 
-          const emitAddTodo = () => {
-            const todo = {
-              id: props.tid,
-              content: todoContent.value,
-              completed: false,
-            };
-            context.emit("add-todo", todo);
-            todoContent.value = "";
-          };
-          return {
-            todoContent,
-            emitAddTodo,
-          };
-        },
+export default {
+  name: "TodoAdd",
+  setup(props, context) {
+    const todoContent = ref("");
+
+    const emitAddTodo = () => {
+      const todo = {
+        id: props.tid,
+        content: todoContent.value,
+        completed: false,
+      };
+      context.emit("add-todo", todo);
+      todoContent.value = "";
     };
+    return {
+      todoContent,
+      emitAddTodo,
+    };
+  },
+};
 </script>
 
 <style>
-.input-add{
+.input-add {
   position: relative;
   display: flex;
   align-items: center;
@@ -42,24 +44,24 @@
   border-radius: 48px;
   border: none;
   outline: none;
-  box-shadow: 0px 0px 24px rgba(0,0,0,0.08);
+  box-shadow: 0px 0px 24px rgba(0, 0, 0, 0.08);
   width: 100%;
   font-size: 16px;
   color: #626262;
 }
 
-.input-add button{
+.input-add button {
   width: 46px;
   height: 46px;
   border-radius: 50%;
-  background: linear-gradient(#c0a5f3,#7f95f7);
+  background: linear-gradient(#c0a5f3, #7f95f7);
   border: none;
   outline: none;
 
   color: white;
   position: absolute;
-  right:0px;
-  
+  right: 0px;
+
   cursor: pointer;
 }
 
@@ -67,11 +69,10 @@
   display: block;
   width: 100%;
   height: 100%;
-  background: linear-gradient(#fff,#fff),linear-gradient(#fff,#fff);
+  background: linear-gradient(#fff, #fff), linear-gradient(#fff, #fff);
   background-size: 100% 1.5px, 1.5px 30%;
   /* background-size: 50% 2px, 2px 50%; */
   background-position: center;
   background-repeat: no-repeat;
 }
-
 </style>
